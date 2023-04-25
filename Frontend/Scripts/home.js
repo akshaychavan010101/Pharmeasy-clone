8a50eb7a539db15e872e199d62e454fa7b84f77f
akshaychavan010101 <akshaychavan010101@gmail.com>
2023-04-25 15:04:21 +0530

    update

--- Frontend/Scripts/home.js
+++ Frontend/Scripts/home.js
@@ -1,15 +1,26 @@
-let arr = ["Medicines","Home care","Child care","Food & Drinks","Hygien"];
+let arr = ["Medicines", "Home care", "Child care", "Food & Drinks", "Hygien"];
 let Question = document.getElementById("Question");
 let i = 0;
-setInterval((()=>{
-    Question.style.color = "green";
-    Question.innerText = arr[i];
-    i++;
-    if(i==arr.length-1){
-        i=0;
-    }
-}),3000)
+setInterval(() => {
+  Question.style.color = "green";
+  Question.innerText = arr[i];
+  i++;
+  if (i == arr.length - 1) {
+    i = 0;
+  }
+}, 3000);
+
+const user_drop_list = document.querySelector("#user_drop_list");
+const userIcon = document.querySelector("#userIcon");
 
 
+userIcon.addEventListener("mouseover", () => {
+  user_drop_list.style.display = "block";
+});
+
+
+userIcon.addEventListener("mouseout", () => {
+  user_drop_list.style.display = "none";
+});
 
 
